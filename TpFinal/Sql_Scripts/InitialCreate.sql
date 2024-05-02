78aff8a0a572a40e0e33bf8959b5959f114c069f
D <2164425@cegepmontpetit.ca>
2024-05-02 09:53:07 -0400

    BugFix: Initiale create

---
+++ TpFinal/Sql_Scripts/InitialCreate.sql
@@ -0,0 +1,39 @@
+USE master 
+GO
+
+-- CREATION ou RECREATION de la BD
+IF
+EXISTS(SELECT * FROM sys.databases WHERE name='Hero')
+BEGIN
+	DROP DATABASE Hero
+END
+
+CREATE DATABASE Hero
+GO
+
+-- Configuration de FILESTREAM
+EXEC sp_configure filestream_access_level, 2 RECONFIGURE
+
+ALTER DATABASE Hero
+ADD FILEGROUP FG_Images2164425 CONTAINS FILESTREAM;
+GO
+
+ALTER DATABASE Hero
+ADD FILE (
+	NAME = FG_Images2164425,
+	FILENAME = 'C:\EspaceLabo\FG_Images2164425'
+	
+)
+TO FILEGROUP FG_Images2164425
+GO
+
+-- Configuration des clés symétriques
+USE Hero
+GO
+
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'CeciEst1GrosMotDePasse!'
+GO
+CREATE CERTIFICATE Hero_Certificat WITH SUBJECT = 'ChiffrementDonneeCarteBancaire'
+GO
+CREATE SYMMETRIC KEY Hero_Cle WITH ALGORITHM = AES_256 ENCRYPTION BY CERTIFICATE Hero_Certificat
+GO
